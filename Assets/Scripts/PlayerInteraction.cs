733199ce97adbba0d930201f61c5c3cf7fd57d47
Ryguyrocks22 <78097100+RyguyRockz@users.noreply.github.com>
2024-10-03 06:31:52 -0400

    Food can be picked up somewhat

--- Assets/Scripts/PlayerInteraction.cs
+++ Assets/Scripts/PlayerInteraction.cs
@@ -1,27 +1,87 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-
 
 public class PlayerInteraction : MonoBehaviour
 {
     public Transform InteractorSource;
     public float InteractRange;
+    public LayerMask pickupLayer; // Layer for items to pick up
+
+    // Inventory system
+    public Transform itemHoldPosition1;
+    public Transform itemHoldPosition2;
+    public float dropOffset = 1f;
+    private Stack<GameObject> inventoryStack = new Stack<GameObject>();
+    private const int maxInventorySize = 2;
 
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.E)) // Check for 'E' key press
         {
-            Ray ray = new Ray(InteractorSource.position, InteractorSource.forward);
+            TryInteractOrPickup();
+        }
 
-            if (Physics.Raycast(ray, out RaycastHit hitInfo, InteractRange))
+        if (Input.GetKeyDown(KeyCode.Q)) // Drop item with 'Q'
+        {
+            TryDropItem();
+        }
+    }
+
+    private void TryInteractOrPickup()
+    {
+        Ray ray = new Ray(InteractorSource.position, InteractorSource.forward);
+
+        // Check if the ray hits anything in range
+        if (Physics.Raycast(ray, out RaycastHit hitInfo, InteractRange))
+        {
+            // Check if it's a pickup item
+            if (hitInfo.collider.CompareTag("Pickup") && inventoryStack.Count < maxInventorySize)
             {
-                if (hitInfo.collider.gameObject.TryGetComponent(out IInteractable interactable))
-                {
-                    interactable.Interact(); // Trigger interaction if the object implements IInteractable
-                }
+                PickupItem(hitInfo.collider.gameObject);
+                return;
             }
+
+            // If not a pickup, check for general interactable objects
+            if (hitInfo.collider.gameObject.TryGetComponent(out IInteractable interactable))
+            {
+                interactable.Interact(); // Interact with non-pickup objects
+            }
+        }
+    }
+
+    private void TryDropItem()
+    {
+        if (inventoryStack.Count == 0) return;
+
+        GameObject itemToDrop = inventoryStack.Pop();
+        Vector3 dropPosition = transform.position + transform.forward * dropOffset;
+        itemToDrop.transform.position = dropPosition;
+        itemToDrop.SetActive(true); // Re-enable the dropped item
+
+        UpdateInventoryDisplay();
+    }
+
+    private void PickupItem(GameObject item)
+    {
+        inventoryStack.Push(item);
+        item.SetActive(false); // Hide the item while in the player's inventory
+        UpdateInventoryDisplay();
+    }
+
+    private void UpdateInventoryDisplay()
+    {
+        // Update positions for the items in the player's inventory (above their head)
+        if (inventoryStack.Count > 0)
+        {
+            var item1 = inventoryStack.Peek();
+            item1.transform.position = itemHoldPosition1.position;
+            item1.SetActive(true);
+        }
+        if (inventoryStack.Count > 1)
+        {
+            var item2 = inventoryStack.ToArray()[1];
+            item2.transform.position = itemHoldPosition2.position;
+            item2.SetActive(true);
         }
     }
 }
